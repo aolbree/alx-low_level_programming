96f4f03c143edf8995652220a0d3d429599ee019
aolbree <aolbrenda@gmail.com>
2023-02-23 14:54:19 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - entry point
+ * main - prints prime factors of 612852475143
  *
- * Return: always 0 (sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int num = 612852475143;
-	unsigned long int prim;
-
-	prim = 3;
-	while (prim < num / 2)
-	{
-		if ((num % prim) == 0)
-		{
-			if((prim % 3) == 2)
-				printf(",%lu ", prim);
-		}
-
-		prim+=2;
-	}
-
-	putchar('\n');
 	return (0);
 }
-
-
