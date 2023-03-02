edfd5cac7c293fc6b2da098daacb0085bba306f8
aolbree <aolbrenda@gmail.com>
2023-03-02 13:36:54 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,29 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_number - Prints an integer
+ * @n: Integer to be printed
  */
-int main(void)
+void print_number(int n)
 {
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	return (0);
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	for (i = 1000000000; i > 0; i /= 10)
+	{
+		if (n / i)
+		{
+			if ((n / i) % 10 < 0)
+				_putchar(-(n / i % 10) + '0');
+			else
+				_putchar((n / i % 10) + '0');
+		}
+		else if (n / i == 0 && i == 1)
+		{
+			_putchar(n / i % 10 + '0');
+		}
+	}
 }
-
-
-
