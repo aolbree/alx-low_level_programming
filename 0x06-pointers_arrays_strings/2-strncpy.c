8f73edcfe45de238941dacafe993f72386b3fbca
aolbree <aolbrenda@gmail.com>
2023-03-02 12:38:06 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * _strncpy - Entry point
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
- * Return: Always 0 (Success)
+ * _strncpy - this fuction will concatenates two strings
+ * @dest: the string that will modified.
+ * @src: the string that ill be concatenated to @des.
+ * @n: how much to copy from src into dest.
+ *
+ * Return: a pointer to @des
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i_01 = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
+	for (i_01 = 0; i_01 < n && src[i_01] != '\0'; i_01++)
+		dest[i_01] = src[i_01];
+	for ( ; i_01 < n ; i_01++)
+		dest[i_01] = '\0';
 
 	return (dest);
 }
+
+
 
