495a2a6836e93cb30068989e2ff3039802924b90
aolbree <aolbrenda@gmail.com>
2023-03-10 16:05:29 -0800

    create_static_lib.sh

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
+int _islower(char c);
+int _isalpha(char c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
